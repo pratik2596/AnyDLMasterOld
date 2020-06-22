d197d2889dddfef871dad2de61ec53a9c2249862
pratik2596 <65121248+pratik2596@users.noreply.github.com>
2020-06-23 02:41:51 +0530

    Update convert_to_video.py

--- plugins/convert_to_video.py
+++ plugins/convert_to_video.py
@@ -107,7 +107,7 @@ async def convert_to_video(bot, update):
             await bot.send_video(
                 chat_id=update.chat.id,
                 video=the_real_download_location,
-                caption=description,
+                caption=file_name,
                 duration=duration,
                 width=width,
                 height=height,
